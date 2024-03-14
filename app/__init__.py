0df0e521745925ac30033eda55424fd3132c9477
ITLAZ <juan.tapia.l@ucb.edu.bo>
2024-03-14 04:08:05 -0400

    index nuevo y arreglo carpeta static

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,12 @@ from flask import Flask
 from pathlib import Path
 from flask_sqlalchemy import SQLAlchemy
 
-template_dir = Path('C:/Users/paola qv/Documents/ToDoList/templates')
+template_dir = Path('../templates')
 app = Flask(__name__, template_folder=template_dir)
+app.static_folder = Path('../static')
 
 app.config['SECRET_KEY'] = 'todolist'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:marceline25@localhost/todolist_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/todolist_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
